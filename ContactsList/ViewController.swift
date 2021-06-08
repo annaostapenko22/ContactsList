00476ad5f0c77af4c548e4df15df4256c9574fb8
Anna Ostapenko <kvintabudinvest@gmail.com>
2021-06-08 22:06:43 +0200

    Initial Commit

---
+++ ContactsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsList
+//
+//  Created by Anna Ostapenko on 08.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
